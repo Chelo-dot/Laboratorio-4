679e7eb2ac48a38e0f961101300998574e863c8f
Chelo <marvillo028@gmail.com>
2026-06-02 16:04:28 -0600

    IMPRESION DE RESULTADOS

--- ej2.c
+++ ej2.c
@@ -6,7 +6,7 @@ unsigned char *read_pgm(const char *filename, int *width, int *height, int *max_
     unsigned char *pixels;
 
     FILE *archivo;
-    archivo = fopen("imagen.pgm", "r");
+    archivo = fopen(filename, "r");
 
 
     // verifica que el archivo exista
@@ -25,6 +25,7 @@ unsigned char *read_pgm(const char *filename, int *width, int *height, int *max_
 
     // reserva memoria
     pixels = (unsigned char*)malloc(*height * *width * sizeof(unsigned char));
+    if (pixels == NULL) return NULL;
 
     
     // guarda los datos de cada pixel
@@ -53,15 +54,16 @@ unsigned char *make_negative(unsigned char *pixels, int total){
     unsigned char *negative;
     negative = (unsigned char*)malloc(total * sizeof(unsigned char));
 
+    if(negative == NULL) {
+        printf("No se pudo encontrar un archivo");
+        return NULL;
+    }
 
     for(int i = 0; i < total; i++){
-
         *(negative + i) = 255 - *(pixels + i);
-
     }
 
     return negative;
-
 }
 
 void write_pgm(const char *filename, unsigned char *pixels, int width, int height, int max_val){
@@ -74,40 +76,73 @@ void write_pgm(const char *filename, unsigned char *pixels, int width, int heigh
     fprintf(archivo, "%d %d\n", width, height);
     fprintf(archivo, "%d\n", max_val);
     for(int i = 0; i < width*height; i++){
-            fprintf(archivo, "%i ", (int)*(pixels +i));
-            
+        fprintf(archivo, "%i ", (int)*(pixels +i));   
     }
 
+    fclose(archivo);
 
 }
 
 void print_stats(unsigned char *original, unsigned char *thresholded, int total){
 
+    int prom = 0;
+    int blanco = 0;
+    int negro = 0;
+
+    for(int i = 0; i < total; i++){
+        prom += (int)*(original + i);
+        if((int)*(thresholded + i) == 0) negro ++;
+        else blanco ++;
+    }
+
+    printf("Cantidad de pixeles blancos: %d\nCantidad de pixeles negros: %d\nValor promedio de los pixeles: %d\n", blanco, negro, prom/total);
+
 }
 
 int main(void){
 
-    const char *filename = "imagen.pgm";
+    const char *filename = "input.pgm";
 
     int width, height, max_val, threshold;
+    unsigned char *original = NULL;
     unsigned char *pixels = NULL;
     unsigned char *negative = NULL;
 
-    pixels = read_pgm(filename, &width, &height, &max_val);
-
+    // leer archivo
+    original = read_pgm(filename, &width, &height, &max_val);
+    if(original == NULL){
+        printf("Erro al abrir el archivo");
+        return 1;
+    }
     int total = width*height;
 
-    negative = make_negative(pixels, total);
+    // copia original en pixels
+    pixels = (unsigned char*)malloc(total * sizeof(unsigned char));
+    for(int i = 0; i < total; i++){
+        *(pixels + i) = *(original + i);
+    }
 
+    // aplicar filtros
+    negative = make_negative(pixels, total);
+    if(negative == NULL) {
+        printf("Error al guardar memoria");
+        return 1;
+    }
 
     printf("Ingrese el valor del threshold que desea aplicar: ");
     scanf("%i", &threshold);
     apply_threshold(pixels, total, threshold);
 
-    write_pgm("threshold.pgm", pixels, width, height, max_val);
-    write_pgm("negative.pgm", negative, width, height, max_val);
+    // guardar archivos
+    write_pgm("output_threshold.pgm", pixels, width, height, max_val);
+    write_pgm("output_negative.pgm", negative, width, height, max_val);
 
+    print_stats(original, pixels, total);
 
+    // liberar espacio
+    free(original);
+    free(pixels);
+    free(negative);
 
     return 0;
 }
