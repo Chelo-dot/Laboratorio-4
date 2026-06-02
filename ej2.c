d707ac7a7204b694d0af8b60ccdb15995a6ea648
Chelo <marvillo028@gmail.com>
2026-06-02 14:23:44 -0600

    APLICAR THRESHOD Y GUARDAR ARCHIVO

--- ej2.c
+++ ej2.c
@@ -3,34 +3,44 @@
 
 unsigned char *read_pgm(const char *filename, int *width, int *height, int *max_val){
 
-    unsigned char *pixeles;
+    unsigned char *pixels;
 
     FILE *archivo;
-    archivo = fopen(*filename, "r");
+    archivo = fopen("/home/chelo/Plataformas/Laboratorio-4/imagen.pgm", "r");
 
 
     // verifica que el archivo exista
     if (archivo == NULL) return NULL;
 
     fscanf(archivo, "%*[^\n]\n");
+    fscanf(archivo, "%*[^\n]\n");
+    // lee el tamano de la imagen
     fscanf(archivo, "%i %i", width, height);
     fscanf(archivo, "%i", max_val);
 
 
     // reserva memoria
-    pixeles = (unsigned char*)malloc(*height * *width * sizeof(unsigned char));
+    pixels = (unsigned char*)malloc(*height * *width * sizeof(unsigned char));
 
+    
+    //guarda los datos de cada pixel
     for(int i = 0; i < *height * *width; i++){
-            fscanf(archivo, "%c", *(pixeles + i));
+            fscanf(archivo, "%i", (pixels + i));
     }
 
     fclose(archivo);
 
-    return pixeles;
+    return pixels;
 
 }
 
 void apply_threshold(unsigned char *pixels, int total, int threshold){
+
+    for(int i = 0; i < total; i++){
+        if((int)*(pixels + i) >= threshold) *(pixels + i) = 255;
+        else *(pixels + i) =  0;
+
+    }
 
 }
 
@@ -40,6 +50,19 @@ unsigned char *make_negative(unsigned char *pixels, int total){
 
 void write_pgm(const char *filename, unsigned char *pixels, int width, int height, int max_val){
 
+    FILE *archivo = fopen("threshold.pgm", "w");
+
+    if (archivo == NULL) return;
+
+    fprintf(archivo, "P2\n");
+    fprintf(archivo, "%d %d\n", width, height);
+    fprintf(archivo, "%d\n", max_val);
+    for(int i = 0; i < width*height; i++){
+            fprintf(archivo, "%i ", (int)*(pixels +i));
+            if (i % width == 0) fprintf(archivo, "\n");
+    }
+
+
 }
 
 void print_stats(unsigned char *original, unsigned char *thresholded, int total){
@@ -48,38 +71,22 @@ void print_stats(unsigned char *original, unsigned char *thresholded, int total)
 
 int main(void){
 
-
-    FILE *archivo;
-    archivo = fopen("/home/chelo/Plataformas/Laboratorio-4/imagen.pgm", "r");
-
-    if (archivo == NULL) return 0;
-
-    unsigned char *pixeles;
-
-
-
+    char *filename = "/home/chelo/Plataformas/Laboratorio-4/imagen.pgm";
 
     int width, height, max_val, threshold;
     unsigned char *pixels = NULL;
     unsigned char *negative = NULL;
 
-    fscanf(archivo, "%*[^\n]\n");
-    fscanf(archivo, "%i %i", &width, &height);
-    fscanf(archivo, "%i", &max_val);
+    pixels = read_pgm("/home/chelo/Plataformas/Laboratorio-4/imagen.pgm", &width, &height, &max_val);
+
+    int total = width*height;
+
+    
+    apply_threshold(pixels, total, 75);
+    write_pgm("threshold.pgm", pixels, width, height, max_val);
 
 
-    // reserva memoria
-    pixeles = (unsigned char*)malloc(height * width * sizeof(unsigned char));
 
-    for(int i = 0; i < height * width; i++){
-            fscanf(archivo, "%c", (pixeles + i));
-    }
-
-    for(int i = 0; i < height * width; i++){
-            printf("%c", *(pixeles + i));
-    }
-
-    fclose(archivo);
 
     return 0;
 }
