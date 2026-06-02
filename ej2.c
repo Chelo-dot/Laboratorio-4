4b0027baa68fe8573da218d7c795b32227e0446c
Chelo <marvillo028@gmail.com>
2026-06-02 14:38:22 -0600

    FUNCION NEGATIVE

--- ej2.c
+++ ej2.c
@@ -13,7 +13,11 @@ unsigned char *read_pgm(const char *filename, int *width, int *height, int *max_
     if (archivo == NULL) return NULL;
 
     fscanf(archivo, "%*[^\n]\n");
+
+    //Se salta una segunda linea en caso de comentarios
     fscanf(archivo, "%*[^\n]\n");
+
+
     // lee el tamano de la imagen
     fscanf(archivo, "%i %i", width, height);
     fscanf(archivo, "%i", max_val);
@@ -23,9 +27,9 @@ unsigned char *read_pgm(const char *filename, int *width, int *height, int *max_
     pixels = (unsigned char*)malloc(*height * *width * sizeof(unsigned char));
 
     
-    //guarda los datos de cada pixel
+    // guarda los datos de cada pixel
     for(int i = 0; i < *height * *width; i++){
-            fscanf(archivo, "%i", (pixels + i));
+            fscanf(archivo, "%hhu", (pixels + i));
     }
 
     fclose(archivo);
@@ -46,11 +50,23 @@ void apply_threshold(unsigned char *pixels, int total, int threshold){
 
 unsigned char *make_negative(unsigned char *pixels, int total){
 
+    unsigned char *negative;
+    negative = (unsigned char*)malloc(total * sizeof(unsigned char));
+
+
+    for(int i = 0; i < total; i++){
+
+        *(negative + i) = 255 - *(pixels + i);
+
+    }
+
+    return negative;
+
 }
 
 void write_pgm(const char *filename, unsigned char *pixels, int width, int height, int max_val){
 
-    FILE *archivo = fopen("threshold.pgm", "w");
+    FILE *archivo = fopen("negative.pgm", "w");
 
     if (archivo == NULL) return;
 
@@ -59,7 +75,7 @@ void write_pgm(const char *filename, unsigned char *pixels, int width, int heigh
     fprintf(archivo, "%d\n", max_val);
     for(int i = 0; i < width*height; i++){
             fprintf(archivo, "%i ", (int)*(pixels +i));
-            if (i % width == 0) fprintf(archivo, "\n");
+            
     }
 
 
@@ -71,19 +87,22 @@ void print_stats(unsigned char *original, unsigned char *thresholded, int total)
 
 int main(void){
 
-    char *filename = "/home/chelo/Plataformas/Laboratorio-4/imagen.pgm";
+    char *filename = "imagen.pgm";
 
     int width, height, max_val, threshold;
     unsigned char *pixels = NULL;
     unsigned char *negative = NULL;
 
-    pixels = read_pgm("/home/chelo/Plataformas/Laboratorio-4/imagen.pgm", &width, &height, &max_val);
+    pixels = read_pgm("imagen.pgm", &width, &height, &max_val);
 
     int total = width*height;
 
+    negative = make_negative(pixels, total);
     
     apply_threshold(pixels, total, 75);
-    write_pgm("threshold.pgm", pixels, width, height, max_val);
+
+
+    write_pgm("threshold.pgm", negative, width, height, max_val);
 
 
 
